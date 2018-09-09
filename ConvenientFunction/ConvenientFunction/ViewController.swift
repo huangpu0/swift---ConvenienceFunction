10529ed27d0f0711fc69a7c76c7ac874b0d62e46
huangpu <huangpu0205@163.com>
2018-09-09 15:27:40 +0800

    9.9

--- ConvenientFunction/ConvenientFunction/ViewController.swift
+++ ConvenientFunction/ConvenientFunction/ViewController.swift
@@ -14,19 +14,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         //使用例子 UILabel UIButton
-        let lab = UILabel.init(text: "测试数据居中", textColor: .cyan, font: 12.0, textAlignment: .center)
+        let lab = UILabel.init(text: "测试数据居中", textColor: .black, font: 12.0, textAlignment: .center)
         lab.frame = CGRect.init(x: 100, y: 100, width: 100, height: 30)
         lab.backgroundColor = .green
         view.addSubview(lab)
         
-        let btn = UIButton.init(title: "按钮", titleColor: .red, font: 15.0)
+        let btn = UIButton.init(title: "按钮", titleColor: .cyan, font: 15.0)
         btn.frame = CGRect.init(x: 100, y: 150, width: 100, height: 50)
         btn.backgroundColor = UIColor.red
+        btn.addTarget(self, action: #selector(test), for: .touchUpInside)
         view.addSubview(btn)
         
         
     }
 
+    //MARK: 按钮响应事件
+    @objc func test() -> Void{
+        let alter = UIAlertController.init(title: "提示", message: "你点击了按钮", preferredStyle: .alert)
+        let action = UIAlertAction.init(title: "ok", style: .cancel) { (action) in
+            print("取消了点击")
+        }
+        alter.addAction(action)
+        self.present(alter, animated: true, completion: nil)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
